77ca673def9cb3cb40e644482fde837e7dfb01a3
beersxp22-web <beersxp22@gmail.com>
2025-08-19 17:03:05 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = 'speech-app-cache-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
