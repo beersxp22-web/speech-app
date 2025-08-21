d1c17c22494ba314ab7c0843192559684d6183af
beersxp22-web <beersxp22@gmail.com>
2025-08-21 10:31:08 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,28 +12,25 @@ const urlsToCache = [
   'https://i.postimg.cc/v8JHdbFV/file-00000000000061f7a3509f32dfcc86f5.png'
 ];
 
-// ติดตั้ง Service Worker และ cache ไฟล์
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+      .then(()=>self.skipWaiting())
   );
 });
 
-// activate SW และลบ cache เก่า
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+      Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
-// ดัก fetch request
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response=>response||fetch(event.request))
   );
 });
